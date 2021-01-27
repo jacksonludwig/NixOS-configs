a0dbe443963b331ba5589aba7d8ee824a3f362f1
Jackson Ludwig <jacksonludwig0@gmail.com>
2021-01-27 18:08:11 -0500

    remove hydra

--- configs/emacs/init.el
+++ configs/emacs/init.el
@@ -208,19 +208,6 @@
   (ivy-rich-mode 1))
 
 
-;; HYDRA SET UP
-(use-package hydra)
-
-(defhydra hydra-text-scale (:timeout 4)
-  "scale text"
-  ("j" text-scale-increase "in")
-  ("k" text-scale-decrease "out")
-  ("f" nil "finished" :exit t))
-
-(jackson/leader-binds
-  "ts" '(hydra-text-scale/body :which-key "scale text"))
-
-
 ;; ORG SETUP
 (use-package org
   :hook
