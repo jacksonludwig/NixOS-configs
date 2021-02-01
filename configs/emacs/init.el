e67595fe0ae07120fe98534c9cb1c1f46503d7dd
Jackson Ludwig <jacksonludwig0@gmail.com>
2021-02-01 13:27:32 -0500

    change dash

--- configs/emacs/init.el
+++ configs/emacs/init.el
@@ -189,10 +189,12 @@
   :config
   (setq dashboard-startup-banner (concat user-emacs-directory "splash.png")
         dashboard-banner-logo-title "Emacs")
-  (setq dashboard-items '((recents  . 5)
-                          (bookmarks . 5)
+  (setq dashboard-items '(
+                          ;; (recents  . 5)
+                          ;; (bookmarks . 5)
                           (projects . 5)
-                          (agenda . 5)))
+                          (agenda . 5)
+                          ))
   (setq dashboard-projects-switch-function 'counsel-projectile-switch-project-by-name)
   (setq dashboard-footer-messages '("Configured by Jackson"))
   (dashboard-setup-startup-hook))
