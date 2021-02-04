17fcc90c130f996160e648c8be9bdcfd283c56ba
Jackson Ludwig <jacksonludwig0@gmail.com>
2021-02-04 17:56:10 -0500

    add lsp setting

--- configs/emacs/init.el
+++ configs/emacs/init.el
@@ -340,6 +340,7 @@
   :config
   (setq gc-cons-threshold 100000000)
   (setq read-process-output-max (* 1024 1024)) ;; 1mb
+  (setq lsp-log-io nil) ;; just in case
   (setq lsp-completion-provider :capf)
   (setq create-lockfiles nil) ;; disable lockfiles because they annoy some LSP
 
