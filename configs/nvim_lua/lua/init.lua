0bf3878d5839d38053ffd262bea5e801e5a88557
Jackson Ludwig <jacksonludwig0@gmail.com>
2021-03-26 09:44:02 -0400

    remove nerd font req

--- configs/nvim_lua/lua/init.lua
+++ configs/nvim_lua/lua/init.lua
@@ -73,11 +73,12 @@ require('packer').startup(function ()
     config = function() require('gitsigns').setup() end
   }
 
-  use {'hoob3rt/lualine.nvim', requires = {'kyazdani42/nvim-web-devicons', opt = true},
+  use {'hoob3rt/lualine.nvim', 
+    disable = false,
+    -- requires = {'kyazdani42/nvim-web-devicons', opt = true},
     config = function()
       local lualine = require "lualine"
       local oceanic_next = require "jackson.oceanic_next.statusline"
-      local colors = require "jackson.oceanic_next.colors"
       lualine.setup({
         sections = {
           lualine_a = {""},
@@ -90,7 +91,8 @@ require('packer').startup(function ()
             {
               "diagnostics",
               sources = {"nvim_lsp"},
-              symbols = {error = " ", warn = " ", info = " "}
+              -- symbols = {error = " ", warn = " ", info = " "}
+              symbols = {error = "E ", warn = "W ", info = "I "}
             }
           }
         },
@@ -98,53 +100,6 @@ require('packer').startup(function ()
           theme = oceanic_next
         }
       }) 
-    end
-  }
-
-  use {'akinsho/nvim-bufferline.lua', requires = 'kyazdani42/nvim-web-devicons',
-    config = function()
-      local bufferline = require('bufferline')
-      local colors = require "jackson.oceanic_next.colors"
-      bufferline.setup {
-        options = {
-          view = "default",
-          numbers = "ordinal",
-          number_style = "",
-          mappings = false,
-          buffer_close_icon = "",
-          modified_icon = "•",
-          close_icon = "",
-          left_trunc_marker = "",
-          right_trunc_marker = "",
-          max_name_length = 18,
-          max_prefix_length = 15,
-          show_buffer_close_icons = false,
-          persist_buffer_sort = true,
-          separator_style = {"", ""},
-          enforce_regular_tabs = false,
-          always_show_bufferline = true
-        },
-        highlights = {
-          modified = {guifg = colors.green, guibg = "#0F1E28"},
-          modified_visible = {guifg = "#3C706F", guibg = "#16242E"},
-          modified_selected = {guifg = colors.cyan, guibg = "#142832"},
-          fill = {guibg = "#0F1E28"},
-          background = {guibg = "#0F1E28", guifg = colors.base04},
-          tab = {guibg = "#0F1E28", guifg = colors.base01},
-          tab_selected = {guibg = "#142832"},
-          tab_close = {guibg = "#0F1E28"},
-          buffer_visible = {guibg = "#16242E"},
-          buffer_selected = {guibg = "#142832", guifg = colors.white, gui = ""},
-          indicator_selected = {guifg = colors.cyan, guibg = "#142832"},
-          separator = {guibg = "#62b3b2"},
-          separator_selected = {guifg = colors.cyan, guibg = "#142832"},
-          separator_visible = {guibg = colors.cyan},
-          duplicate = {guibg = "#0F1E28", guifg = colors.base04, gui = ""},
-          duplicate_selected = {guibg = "#142832", gui = "", guifg = colors.white},
-          duplicate_visible = {guibg = "#16242E", gui = ""}
-        }
-      }
-      vim.api.nvim_set_keymap('n', 'gb', '<cmd>BufferLinePick<CR>', { noremap = false, silent = true })
     end
   }
 
