8f9c462aa9536221c47d16b480c2d9f26bbc7793
Jackson Ludwig <jludwig@greenactionstudio.com>
2021-06-05 17:36:26 -0400

    try virtual text

--- configs/nvim_lua/lua/init.lua
+++ configs/nvim_lua/lua/init.lua
@@ -215,7 +215,7 @@ nvim_lsp.diagnosticls.setup {
 
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
   vim.lsp.diagnostic.on_publish_diagnostics, {
-    virtual_text = false,
+    virtual_text = true,
   }
 )
 
