a7d17928b26f8b8844cc44e33ca07f54d0521dc8
Jackson Ludwig <jludwig@greenactionstudio.com>
2021-06-07 22:12:16 -0400

    try coc cause tsserver sucks

---
+++ configs/nvim_coc/lua/init.lua
@@ -0,0 +1,114 @@
+------------------ PACKER INSTALL --------------------------
+local execute = vim.api.nvim_command
+local fn = vim.fn
+
+local install_path = fn.stdpath('data')..'/site/pack/packer/opt/packer.nvim'
+
+if fn.empty(fn.glob(install_path)) > 0 then
+  execute('!git clone https://github.com/wbthomason/packer.nvim '..install_path)
+  execute 'packadd packer.nvim'
+end
+
+-------------------- PLUGINS -------------------------------
+vim.cmd [[packadd packer.nvim]]
+require('packer').startup(function ()
+  use {
+    'wbthomason/packer.nvim',
+    opt = true,
+  }
+
+  use {
+    'tpope/vim-commentary',
+  }
+
+  use {
+    'mhartington/oceanic-next',
+  }
+
+  use {
+    'nvim-telescope/telescope.nvim',
+    requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}},
+
+    config = function() 
+      -- This disables tree-sitter highlighting in previewers. Workaround for slowness currently.
+      local previewers = require('telescope.previewers')
+      local putils = require('telescope.previewers.utils')
+      local pfiletype = require('plenary.filetype')
+
+      local new_maker = function(filepath, bufnr, opts)
+        opts = opts or {}
+        if opts.use_ft_detect == nil then
+          local ft = pfiletype.detect(filepath)
+          -- Here for example you can say: if ft == "xyz" then this_regex_highlighing else nothing end
+          opts.use_ft_detect = false
+          putils.regex_highlighter(bufnr, ft)
+        end
+        previewers.buffer_previewer_maker(filepath, bufnr, opts)
+      end
+
+      require('telescope').setup {
+        defaults = {
+          buffer_previewer_maker = new_maker,
+        }
+      }
+
+      vim.api.nvim_set_keymap('n', '<space><space>', '<cmd>lua require("telescope.builtin").find_files()<cr>', { noremap = true, silent = true })
+      vim.api.nvim_set_keymap('n', '<space>fr', '<cmd>lua require("telescope.builtin").oldfiles()<cr>', { noremap = true, silent = true })
+      vim.api.nvim_set_keymap('n', '<space>bb', '<cmd>lua require("telescope.builtin").buffers()<cr>', { noremap = true, silent = true })
+      vim.api.nvim_set_keymap('n', '<space>sg', '<cmd>lua require("telescope.builtin").live_grep()<cr>', { noremap = true, silent = true })
+      vim.api.nvim_set_keymap('n', '<space>ff', '<cmd>lua require("telescope.builtin").file_browser()<cr>', { noremap = true, silent = true })
+    end
+  }
+
+  use {
+    'airblade/vim-rooter',
+  }
+
+  use {
+    'nvim-treesitter/nvim-treesitter', run = ':TSUpdate',
+  }
+
+  use {
+    'peitalin/vim-jsx-typescript',
+  }
+
+  use {
+    'neoclide/coc.nvim', branch = 'release',
+  }
+
+end)
+
+-------------------- VARIABLES -------------------------------
+vim.cmd('set undodir=$HOME/.config/nvim/undodir')
+vim.cmd('set undofile')
+vim.cmd('set hidden')
+vim.cmd('set termguicolors')
+vim.cmd('set splitright')
+vim.cmd('set splitbelow')
+vim.cmd('set nowrap')
+vim.cmd('set signcolumn=yes')
+vim.cmd('set tabstop=2')
+vim.cmd('set shiftwidth=2')
+vim.cmd('set expandtab')
+vim.cmd('set pumheight=40')
+vim.cmd('set number')
+vim.cmd('set relativenumber')
+vim.cmd('set mouse=a')
+vim.cmd('set completeopt=menuone,noselect')
+
+-------------------- MAPPINGS -------------------------------
+vim.api.nvim_set_keymap('n', '<esc>', '<cmd>noh<CR>', { noremap = false, silent = true })
+
+-------------------- THEME -------------------------------
+vim.cmd('colorscheme OceanicNext')
+
+-- TREESITTER
+require'nvim-treesitter.configs'.setup {
+  ensure_installed = "maintained",
+  highlight = {
+    enable = false
+  },
+  indent = {
+    enable = false
+  },
+}
