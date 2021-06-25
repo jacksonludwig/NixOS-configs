c7b437e9cd05ba6d7b0a1ac8014e8e97e6d5604d
Jackson Ludwig <jacksonludwig0@gmail.com>
2021-06-24 22:52:19 -0400

    start lua lsp stuff

---
+++ configs/nvim_lua/lua/init.lua
@@ -0,0 +1,236 @@
+------------------ PACKER INSTALL --------------------------
+local execute = vim.api.nvim_command
+local fn = vim.fn
+
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+
+if fn.empty(fn.glob(install_path)) > 0 then
+  fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
+  execute 'packadd packer.nvim'
+end
+
+-------------------- PLUGINS -------------------------------
+local packer = require('packer')
+
+packer.init({
+  luarocks = {
+    python_cmd = 'python3'
+  }
+})
+
+packer.startup(function ()
+  use {
+    'wbthomason/packer.nvim',
+  }
+
+  use {
+    'tpope/vim-commentary',
+    'tpope/vim-fugitive',
+    'tpope/vim-rhubarb',
+  }
+
+  use {
+    'mkitt/tabline.vim',
+  }
+
+  use {
+    'arzg/vim-colors-xcode',
+  }
+
+  use {
+    'tjdevries/gruvbuddy.nvim',
+    requires = { 'tjdevries/colorbuddy.nvim' },
+  }
+
+  use {
+    'nvim-lua/popup.nvim',
+    'nvim-lua/plenary.nvim',
+    'kyazdani42/nvim-web-devicons',
+  }
+
+  use {
+    'nvim-telescope/telescope.nvim',
+    disable = false,
+    config = function() 
+      -- This disables tree-sitter highlighting in previewers. Workaround for slowness currently.
+      local previewers = require('telescope.previewers')
+      local putils = require('telescope.previewers.utils')
+      local pfiletype = require('plenary.filetype')
+
+      local new_maker = function(filepath, bufnr, opts)
+        opts = opts or {}
+        if opts.use_ft_detect == nil then
+          local ft = pfiletype.detect(filepath)
+          -- Here for example you can say: if ft == "xyz" then this_regex_highlighing else nothing end
+          opts.use_ft_detect = false
+          putils.regex_highlighter(bufnr, ft)
+        end
+        previewers.buffer_previewer_maker(filepath, bufnr, opts)
+      end
+
+      require('telescope').setup {
+        defaults = {
+          buffer_previewer_maker = new_maker,
+        }
+      }
+
+      vim.api.nvim_set_keymap('n', '<space><space>', '<cmd>lua require("telescope.builtin").find_files()<cr>', { noremap = true, silent = true })
+      vim.api.nvim_set_keymap('n', '<space>fr', '<cmd>lua require("telescope.builtin").oldfiles()<cr>', { noremap = true, silent = true })
+      vim.api.nvim_set_keymap('n', '<space>bb', '<cmd>lua require("telescope.builtin").buffers()<cr>', { noremap = true, silent = true })
+      vim.api.nvim_set_keymap('n', '<space>g', '<cmd>lua require("telescope.builtin").live_grep()<cr>', { noremap = true, silent = true })
+      vim.api.nvim_set_keymap('n', '<space>ff', '<cmd>lua require("telescope.builtin").file_browser()<cr>', { noremap = true, silent = true })
+      vim.api.nvim_set_keymap('n', '<space>vc', '<cmd>lua require("telescope.builtin").git_commits()<cr>', { noremap = true, silent = true })
+      vim.api.nvim_set_keymap('n', '<space>vb', '<cmd>lua require("telescope.builtin").git_branches()<cr>', { noremap = true, silent = true })
+      vim.api.nvim_set_keymap('n', '<space>vs', '<cmd>lua require("telescope.builtin").git_status()<cr>', { noremap = true, silent = true })
+    end
+  }
+
+  use {
+    'airblade/vim-rooter',
+  }
+
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    requires = {'JoosepAlviste/nvim-ts-context-commentstring'},
+    run = ':TSUpdate',
+    config = function()
+      require'nvim-treesitter.configs'.setup {
+        ensure_installed = "maintained",
+        highlight = {
+          enable = true,
+          disable = {"typescript", "typescriptreact"}
+        },
+        indent = {
+          enable = false
+        },
+        context_commentstring = {
+          enable = true
+        }
+      }
+    end
+  }
+
+  use {
+    'peitalin/vim-jsx-typescript',
+  }
+
+  use {
+    'tjdevries/express_line.nvim',
+    disable = false,
+    config = function() 
+      local builtin = require('el.builtin')
+      local extensions = require('el.extensions')
+      local sections = require('el.sections')
+      local subscribe = require('el.subscribe')
+
+      local git_icon = subscribe.buf_autocmd("el_file_icon", "BufRead", function(_, bufnr)
+        local icon = extensions.file_icon(_, bufnr)
+        if icon then
+          return icon .. " "
+        end
+
+        return ""
+      end)
+
+      local git_branch = subscribe.buf_autocmd("el_git_branch", "BufEnter", function(window, buffer)
+        local branch = extensions.git_branch(window, buffer)
+        if branch then
+          return " " .. extensions.git_icon() .. " " .. branch
+        end
+      end)
+
+      local git_changes = subscribe.buf_autocmd("el_git_changes", "BufWritePost", function(window, buffer)
+        return extensions.git_changes(window, buffer)
+      end)
+
+      require('el').setup{
+        generator = function(win,buf)
+          return {
+            git_branch,
+            " ",
+            sections.split,
+            git_icon,
+            sections.maximum_width(builtin.responsive_file(140, 90), 0.30),
+            sections.collapse_builtin {
+              " ",
+              builtin.modified_flag,
+            },
+            sections.split,
+            -- coc_diag_wrapper(win, buf),
+            -- lsp status here
+            git_changes,
+            "[",
+            builtin.line_with_width(3),
+            ":",
+            builtin.column_with_width(2),
+            "]",
+            sections.collapse_builtin {
+              "[",
+              builtin.help_list,
+              builtin.readonly_list,
+              "]",
+            },
+            builtin.filetype,
+          }
+        end
+      }
+    end
+  }
+
+  use {
+    'neovim/nvim-lspconfig',
+    requires = { 'jose-elias-alvarez/nvim-lsp-ts-utils' },
+    config = function()
+    end
+  }
+
+end)
+
+-------------------- VARIABLES -------------------------------
+local opt = vim.opt
+
+opt.undofile = true
+opt.hidden = true
+opt.termguicolors = true
+opt.splitright = true
+opt.splitbelow = true
+opt.wrap = false
+opt.signcolumn = "yes"
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
+opt.number = true
+opt.relativenumber = true
+opt.mouse = 'a'
+opt.completeopt = { 'menuone', 'noselect', 'noinsert' }
+opt.foldmethod = 'marker'
+opt.foldlevel = 0
+opt.modelines = 1
+
+-- Highlight on yank
+vim.api.nvim_exec([[
+augroup YankHighlight
+autocmd!
+autocmd TextYankPost * silent! lua vim.highlight.on_yank()
+augroup end
+]], false)
+
+-- autocmd for theme stuff
+vim.api.nvim_exec([[
+function SetColors()
+  :hi VertSplit guifg=bg guibg=bg
+  :hi SignColumn guibg=bg
+  :hi LineNr guibg=bg
+  endfunction
+
+  augroup minimal_theme
+  autocmd!
+  autocmd ColorScheme * call SetColors()
+  augroup END
+  ]], false)
+
+  -------------------- MAPPINGS -------------------------------
+  vim.api.nvim_set_keymap('n', '<esc>', '<cmd>noh<CR>', { noremap = false, silent = true })
+
+  -------------------- THEME -------------------------------
+  require('colorbuddy').colorscheme('gruvbuddy')
--- user/jacksonWSL.nix
+++ user/jacksonWSL.nix
@@ -32,6 +32,7 @@
 
     gcc
     cmake
+    go
 
     # nodejs
     nodejs-16_x
@@ -72,13 +73,13 @@
     enable = true;
     package = pkgs.neovim-nightly;
     withNodeJs = true;
-    extraConfig = builtins.readFile ../configs/nvim_coc/init.vim;
+    extraConfig = builtins.readFile ../configs/nvim_lua/init.vim;
   };
   home.file.".config/nvim/lua" = {
-    source = ../configs/nvim_coc/lua;
+    source = ../configs/nvim_lua/lua;
     recursive = true;
   };
-  home.file.".config/nvim/coc-settings.json".source = ../configs/nvim_coc/coc-settings.json;
+  # home.file.".config/nvim/coc-settings.json".source = ../configs/nvim_coc/coc-settings.json;
 
   programs.emacs = {
     enable = true;
